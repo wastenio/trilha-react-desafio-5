14eee2b79fb8d5877bd1786d40f449fafa6f710a
wastenio silva <wastenio.silva@gmail.com>
2024-01-19 15:09:57 -0300

    Complementaão do desafio.

--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -11,9 +11,18 @@ export const getPosts = async () => {
 }
 
 export const getPostBySlug = async (id) => {
+    try {
+        const { data } = await api.get(`/post?id=eq.${id}`);
 
-    //TODO: BUSCAR UM POST EM ESPECIFICO.
-    //const {data} = await api.get(`/post?id=eq.${id}`)
-
-    return {}
+        if (data && data.length > 0) {
+            // Se encontrar o post, retorna o primeiro elemento do array (supondo que seja único)
+            return data[0];
+        } else {
+            // Se não encontrar nenhum post, pode retornar um objeto vazio ou lançar uma exceção, dependendo do seu requisito.
+            return {};
+        }
+    } catch (error) {
+        console.error('Erro ao buscar o post por ID:', error);
+        throw error; // Você pode escolher tratar o erro de outra forma, dependendo do seu caso de uso.
+    }
 }
